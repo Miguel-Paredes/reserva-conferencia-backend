6cd7559f036e956945fa9bed625949808bca19a9
AzureAD\10800 <os.miguelparedes@gmail.com>
2024-03-24 16:17:03 -0500

    quitar espacios

--- src/models/auditorios.js
+++ src/models/auditorios.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const auditorioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, unique : true},
-  capacidad: { type: Number, required: true },
-  ubicacion: { type: String, required: true }
+  nombre: { type: String, required: true, unique : true, trim : true },
+  capacidad: { type: Number, required: true, trim : true },
+  ubicacion: { type: String, required: true, trim : true }
 });
 
 export default mongoose.model("Auditorio", auditorioSchema);;
--- src/models/reservas.js
+++ src/models/reservas.js
@@ -2,8 +2,8 @@ import mongoose, { mongo } from "mongoose";
 
 const reservaSchema = new mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    codigo : { type : String, require : true, unique : true },
-    descripcion : { type : String },
+    codigo : { type : String, require : true, unique : true, trim : true},
+    descripcion : { type : String, trim : true },
     conferencista : {
         type : mongoose.Schema.Types.String,
         ref : 'Conferencista',
@@ -24,7 +24,7 @@ const reservaSchema = new mongoose.Schema({
         ref : 'Auditorio',
         require : true
     },
-    fecha : { type : Date , require : true}
+    fecha : { type : Date , require : true, trim : true }
 })
 
 export default mongoose.model('Reserva', reservaSchema)
